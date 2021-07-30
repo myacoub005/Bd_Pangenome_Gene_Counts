836f6939c226043d62b2cd05c7adf5519008f5ea
Mark Yacoub <68869747+myacoub005@users.noreply.github.com>
2021-07-30 09:53:06 -0700

    Create 00_download.sh
    Download the Hmms

---
+++ 00_download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p short -N 1 -n 16 --mem 16gb --out logs/download.log
+
+# Download the hmms we'll need for the Hmmsearches
+
+curl -o Peptidase_S41.hmm http://pfam.xfam.org/family/PF03572/hmm # see http://pfam.xfam.org/family/PF03572 for  domain
+curl -o Peptidase_M36.hmm http://pfam.xfam.org/family/PF02128/hmm # Peptidase M36
+curl -o Asp_protease.hmm http://pfam.xfam.org/family/PF09668/hmm # Aspartyl protease
+curl -o CBM18.hmm http://pfam.xfam.org/family/PF00187/hmm #CBM18
+
+# Now let's move our Hmms to their own sub-directories to stay organized
+
+for file in *.hmm; do dir=$(echo $file | cut -d. -f1); mkdir -p $dir; mv $file $dir; done
